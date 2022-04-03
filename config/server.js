7c1eafca123c3d5265572daeabcbbee112be674a
Abdullah Alhusaini <arvg3a@gmail.com>
2022-04-03 22:47:38 +0300

    removed unnecessary procfile and reverted to port 1337 to avoid errors

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => {
   return {
     host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 2000),
+    port: env.int("PORT", 1337),
     app: {
       keys: env.array("APP_KEYS"),
     },
